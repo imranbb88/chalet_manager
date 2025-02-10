087828159533cd6ccd2dd0aa5c78ed962e6613d3
imranbb88 <imran@hisparservices.com>
2025-02-10 20:41:35 +0500

    fixes for vercel

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -35,7 +35,7 @@ export default function DashboardPage() {
 
   useEffect(() => {
     fetchDashboardData();
-  }, []);
+  }, [fetchDashboardData]);
 
   async function fetchDashboardData() {
     try {
